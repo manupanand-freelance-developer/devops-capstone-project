1e854d2f10b0d4c46d4574dc0ad6154599845600
manupanand <manupanandindianinvestor@gmail.com>
2025-01-27 22:25:09 +0530

    edit hostname

--- service/config.py
+++ service/config.py
@@ -12,7 +12,7 @@ if not DATABASE_URI:
     DATABASE_USER = os.getenv("DATABASE_USER", "postgres")
     DATABASE_PASSWORD = os.getenv("DATABASE_PASSWORD", "postgres")
     DATABASE_NAME = os.getenv("DATABASE_NAME", "postgres")
-    DATABASE_HOST = os.getenv("DATABASE_HOST", "postgres")# local host changed to postgres
+    DATABASE_HOST = os.getenv("DATABASE_HOST", "localhost")# local host changed to postgres
     DATABASE_URI = f"postgresql://{DATABASE_USER}:{DATABASE_PASSWORD}@{DATABASE_HOST}:5432/{DATABASE_NAME}"
 
 # Configure SQLAlchemy
